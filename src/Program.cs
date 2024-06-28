02f8653835ffad24e5832da8be4f83c238979aed
LeonW <leon_1607@hotmail.de>
2024-06-28 23:56:47 +0200

    released v1.0.0 - test9

--- src/Program.cs
+++ src/Program.cs
@@ -1,7 +1,4 @@
-using Squirrel;
 using System;
-using System.Diagnostics;
-using System.IO;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -13,45 +10,11 @@ namespace Toggle_SteamVR.src
         static async Task Main()
         {
             const string releasesUrl = "https://raw.githubusercontent.com/SoBo7a/Toggle_SteamVR/development/Releases/";
-            using (var mgr = new UpdateManager(releasesUrl))
-            {
-                var releaseEntry = await mgr.UpdateApp();
 
-                if (releaseEntry != null)
-                {
-                    MessageBox.Show("Updated to Version:\n" + releaseEntry?.Version, "Toggle SteamVR", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                    RestartApplication(releaseEntry?.Version.ToString());
-                    return; // Exit Main method to prevent further execution
-                }
-            }
+            await Updater.CheckAndUpdate(releasesUrl);
 
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
-        }
-
-        private static void RestartApplication(string newVersion)
-        {
-            string currentExecutablePath = Application.ExecutablePath;
-            string currentDirectory = Path.GetDirectoryName(currentExecutablePath);
-            string newExecutablePath = Path.Combine(currentDirectory, "..", $"app-{newVersion}", "Toggle_SteamVR.exe");
-
-            string batchFilePath = Path.Combine(Path.GetTempPath(), "restart.bat");
-
-            using (StreamWriter writer = new StreamWriter(batchFilePath))
-            {
-                writer.WriteLine("@echo off");
-                writer.WriteLine("timeout /t 0.5 /nobreak > nul");
-                writer.WriteLine("taskkill /f /im Toggle_SteamVR.exe");
-                writer.WriteLine("timeout /t 0.5 /nobreak > nul");
-                writer.WriteLine($"start \"\" \"{newExecutablePath}\"");
-            }
-
-            Process.Start(new ProcessStartInfo()
-            {
-                FileName = batchFilePath,
-                UseShellExecute = false,
-                CreateNoWindow = true
-            });
         }
     }
 }
---
+++ src/Updater.cs
@@ -0,0 +1,62 @@
+﻿using Squirrel;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Toggle_SteamVR.src
+{
+    internal static class Updater
+    {
+        public static async Task CheckAndUpdate(string releasesUrl)
+        {
+            try
+            {
+                using (var mgr = new UpdateManager(releasesUrl))
+                {
+                    var releaseEntry = await mgr.UpdateApp();
+
+                    if (releaseEntry != null)
+                    {
+                        MessageBox.Show($"Updated to Version:\n{releaseEntry.Version}", "Toggle SteamVR", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        RestartApplication(releaseEntry.Version.ToString());
+                        return; // Exit method to prevent further execution
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failed to check for updates: {ex.Message}", "Update Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private static void RestartApplication(string newVersion)
+        {
+            string currentExecutablePath = Application.ExecutablePath;
+            string currentDirectory = Path.GetDirectoryName(currentExecutablePath);
+            string newExecutablePath = Path.Combine(currentDirectory, "..", $"app-{newVersion}", "Toggle_SteamVR.exe");
+
+            string batchFilePath = Path.Combine(Path.GetTempPath(), "restart.bat");
+
+            using (StreamWriter writer = new StreamWriter(batchFilePath))
+            {
+                writer.WriteLine("@echo off");
+                writer.WriteLine("timeout /t 0.5 /nobreak > nul");
+                writer.WriteLine("taskkill /f /im Toggle_SteamVR.exe");
+                writer.WriteLine("timeout /t 0.5 /nobreak > nul");
+                writer.WriteLine($"start \"\" \"{newExecutablePath}\"");
+            }
+
+            Process.Start(new ProcessStartInfo()
+            {
+                FileName = batchFilePath,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            });
+
+            // Exit the current process
+            Application.Exit();
+        }
+    }
+}
