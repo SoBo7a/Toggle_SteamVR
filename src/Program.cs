419ec55b78ec1c8672c1da142e33d096e1b02ba5
LeonW <leon_1607@hotmail.de>
2024-06-28 20:15:21 +0200

    released v1.1.2 - test

--- src/Program.cs
+++ src/Program.cs
@@ -16,7 +16,7 @@ namespace Toggle_SteamVR.src
             {
                 // Check for updates
                 var releaseEntry = await mgr.UpdateApp();
-                MessageBox.Show("Checking for update... " + releaseEntry?.Version, "Update Check", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                MessageBox.Show("Checking for updateZZZ... " + releaseEntry?.Version, "Update Check", MessageBoxButtons.OK, MessageBoxIcon.Information);
 
 
                 if (releaseEntry != null)
