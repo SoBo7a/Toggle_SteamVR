b361bdada8cfccbbdffef747bef534e73ca1fb59
LeonW <leon_1607@hotmail.de>
2024-06-28 21:54:03 +0200

    released v1.0.2 - test3

--- src/Program.cs
+++ src/Program.cs
@@ -1,5 +1,7 @@
 using Squirrel;
 using System;
+using System.Diagnostics;
+using System.IO;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -10,21 +12,41 @@ namespace Toggle_SteamVR.src
         [STAThread]
         static async Task Main()
         {
-            // Initialize Squirrel update manager with your GitHub releases URL
             const string releasesUrl = "https://raw.githubusercontent.com/SoBo7a/Toggle_SteamVR/development/Releases/";
             using (var mgr = new UpdateManager(releasesUrl))
             {
-                // Check for updates
                 var releaseEntry = await mgr.UpdateApp();
 
                 if (releaseEntry != null)
                 {
                     MessageBox.Show("Updated to Version: " + releaseEntry?.Version, "Update Check", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                }            
-            }                       
+                    RestartApplication();
+                }
+            }
 
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
+        }
+
+        private static void RestartApplication()
+        {
+            string batchFilePath = Path.Combine(Path.GetTempPath(), "restart.bat");
+
+            using (StreamWriter writer = new StreamWriter(batchFilePath))
+            {
+                writer.WriteLine("@echo off");
+                writer.WriteLine("timeout /t 1 /nobreak > nul");
+                writer.WriteLine("start \"\" \"" + Application.ExecutablePath + "\"");
+            }
+
+            Process.Start(new ProcessStartInfo()
+            {
+                FileName = batchFilePath,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            });
+
+            Environment.Exit(0);
         }
     }
 }
