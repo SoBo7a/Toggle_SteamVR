512b2e921e36174f11070aefe6eb151344935a49
LeonW <leon_1607@hotmail.de>
2024-06-29 15:08:30 +0200

    prevent starting multiple instances; prevent update if not installed

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+## v1.0.1
+- prevent running multiple instances of the app at once
+- code refactoring
+
+
+## v1.0.0
+- initial release
--- src/Program.cs
+++ src/Program.cs
@@ -6,13 +6,42 @@ namespace Toggle_SteamVR.src
 {
     internal static class Program
     {
+        private static Mutex mutex = null;
+
         [STAThread]
         static async Task Main()
         {
-            await Updater.CheckAndUpdate();
+            const string appName = "Toggle_SteamVR";
+            bool createdNew;
+
+            mutex = new Mutex(true, appName, out createdNew);
+
+            if (!createdNew)
+            {
+                MessageBox.Show("The application is already running.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Check if the app is running from the installed location or the built exe is used for development
+            if (IsRunningFromInstalledLocation())
+            {
+                await Updater.CheckAndUpdate();
+            }
 
             ApplicationConfiguration.Initialize();
             Application.Run(new NotificationForm());
+
+            GC.KeepAlive(mutex);
+        }
+
+        private static bool IsRunningFromInstalledLocation()
+        {
+            string exePath = Application.ExecutablePath;
+            string exeDirectory = Path.GetDirectoryName(exePath);
+
+            string installedLocation = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\ToggleSteamVR";
+
+            return exeDirectory.StartsWith(installedLocation, StringComparison.OrdinalIgnoreCase);
         }
     }
 }
